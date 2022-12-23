0287c007c02b54d1a971b19ca52a3654233093c1
Tomus <tom.leitner@outlook.de>
2022-12-23 15:06:48 +0100

    Player Idle Animation

--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,5 @@
-class Sprite{
-    constructor({position, imageSrc, scale = 1, framesMax = 1}) {
+class Sprite{ 
+    constructor({position, imageSrc, scale = 1, framesMax = 1, offset = {x: 0, y: 0}}) {
         this.position = position 
         this.width = 50
         this.height = 150
@@ -10,6 +10,7 @@ class Sprite{
         this.framesCurrent = 0
         this.framesElapsed = 0
         this.framesHold = 6
+        this.offset = offset
         
     }
 
@@ -22,19 +23,15 @@ class Sprite{
             0,
             this.image.width / this.framesMax,
             this.image.height,
-            this.position.x, 
-            this.position.y, 
+            this.position.x - this.offset.x, 
+            this.position.y - this.offset.y, 
             (this.image.width / this.framesMax ) * this.scale, 
             this.image.height * this.scale
             )
     }
 
 
-
-
-
-    update(){
-        this.draw()
+    animateFrames(){
         this.framesElapsed++
 
         if(this.framesElapsed % this.framesHold === 0){
@@ -45,15 +42,29 @@ class Sprite{
             this.framesCurrent = 0
 
         }
+
+    }
+
+
+    update(){
+        this.draw()
+        this.animateFrames()
     }
 
 }
 
 
 
-class Fighter{
-    constructor({position, velocity, color = 'red', offset}) {
-        this.position = position 
+class Fighter extends Sprite{
+    constructor({position, velocity, color = 'red', imageSrc, scale = 1, framesMax = 1, offset = {x: 0, y: 0}}) {
+        super({
+            position,
+            imageSrc,
+            scale,
+            framesMax,
+            offset
+        })
+       
         this.velocity = velocity
         this.width = 50
         this.height = 150
@@ -70,25 +81,16 @@ class Fighter{
         this.color = color
         this.isAttacking 
         this.health = 100
+        this.framesCurrent = 0
+        this.framesElapsed = 0
+        this.framesHold = 6
         
     }
-    draw(){
-
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    
-                
-        // Attack Box
-        if(this.isAttacking){
-            c.fillStyle = 'green'
-            c.fillRect(this.attackBox.position.x, this.attackBox.position.y,this.attackBox.height, this.attackBox.width);
-            
-        }
-        
-    }
-
+  
     update(){
         this.draw()
+        this.animateFrames()
+
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
         this.attackBox.position.y = this.position.y
 
