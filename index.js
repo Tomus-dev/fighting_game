2f96161acdbe2208f028a32d8f8dc6bc0fa7db2a
Tomus <tom.leitner@outlook.de>
2022-12-23 15:07:18 +0100

    Idle Animation

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ const player = new Fighter({
     offset: {
         x: 0, 
         y: 0
+    },
+    imageSrc: './img/samuraiMack/Idle.png',
+    framesMax: 8,
+    scale: 2.5, 
+    offset:{
+        x: 215, 
+        y: 157
     }
 })
 
@@ -110,7 +117,7 @@ function animate(){
     background.update()
     shop.update()
     player.update()
-    enemy.update()
+    //enemy.update()
     
     player.velocity.x = 0
     enemy.velocity.x = 0
